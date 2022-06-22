4cb10ea11fad3a64f186e32dd97b4b558bda95cf
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2022-06-22 15:54:23 -0300

    Exercicio 02

--- trybe-exercicios/fundamentos/bloco-08-ES6/dia-01-hofs/exercicio-01/script.js
+++ trybe-exercicios/fundamentos/bloco-08-ES6/dia-01-hofs/exercicio-01/script.js
@@ -1,15 +1,15 @@
 const employeeID = (nome) => {
   return {
     nomeCompleto: nome,
-    email: `${nome}@trybe.com`,
+    email: `${nome.toLowerCase().split(' ').join('_')}@trybe.com`,
   }
 };
 
-const newEmployees = (callback, name) => {
+const newEmployees = (callback) => {
   const employees = {
-    id1: callback(name), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: callback(name), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: callback(name), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
