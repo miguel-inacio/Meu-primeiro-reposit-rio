c6ef4397362259cbc75651ea1ff60b14d9c8432e
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2022-07-21 18:42:42 -0300

    Exercícios para fixar

--- trybe-exercicios/front-end/bloco-11-Componentes com Estado, Eventos e Formulários com React/dia-01-componentes-com-estado-e-eventos/exercicio-para-fixar/fixando-evento/src/Button.jsx
+++ trybe-exercicios/front-end/bloco-11-Componentes com Estado, Eventos e Formulários com React/dia-01-componentes-com-estado-e-eventos/exercicio-para-fixar/fixando-evento/src/Button.jsx
@@ -3,15 +3,36 @@ import React from 'react';
 
 
 class Button extends React.Component {
-  render() {
-    const { innerText } = this.props;
-    function handleClick() {
-      console.log(innerText);
+  constructor() {
+    super()
+    // this.handleClick = this.handleClick.bind(this)
+    this.state = {
+      numeroDeCliques: 0,
     }
+  }
+
+  // const { innerText } = this.props;
+  handleClick = () => {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }));
+    console.log(this.setButtonColor(this.state.numeroDeCliques));
+  };
+
+
+  setButtonColor = (numeroDeCliques) => {
+    if(numeroDeCliques > 0 && numeroDeCliques % 2 === 0) {
+      return 'green';
+    }
+    return 'white';
+  }
+  
+  render() {
     return (
-      <button onClick={ handleClick }>{ innerText }</button>
+      <button onClick={ this.handleClick } style={{ backgroundColor: this.setButtonColor(this.state.numeroDeCliques) }}>Botão</button>
     )
   }
+
 }
 
 export default Button;
